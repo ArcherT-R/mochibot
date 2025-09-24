625c5a481fb427cb4aaf1b2f8d6a58e1564a5709
Archer <archertrenwith@gmail.com>
2025-09-25 08:19:51 +1000

    Update client.js

--- bot/client.js
+++ bot/client.js
@@ -20,7 +20,7 @@ async function startBot() {
   const commands = [];
 
   // Load commands from ./commands
-  const commandsPath = path.join(__dirname, '../commands');
+  const commandsPath = path.join(process.cwd(), 'commands');
   const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
   for (const file of commandFiles) {
