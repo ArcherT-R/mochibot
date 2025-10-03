585b0e9c4850c786057600a4c572aedbc9ad1f71
Archer <archertrenwith@gmail.com>
2025-10-03 15:30:54 +1000

    Update settings.js

--- endpoints/settings.js
+++ endpoints/settings.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const requireLogin = require("../../middleware/requireLogin");
+const requireLogin = require("../middleware/requireLogin");
 const {
   getPlayerLabels,
   addPlayerLabel,
