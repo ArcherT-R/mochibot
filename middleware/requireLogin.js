74760831dd48ac163fd2633d7a16044a4ec5fcb0
Archer <archertrenwith@gmail.com>
2025-11-14 18:52:27 +1100

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -4,13 +4,3 @@ module.exports = function requireLogin(req, res, next) {
   res.redirect('/loginpage/login');
 };
 
-// ========================
-
-// ===================================
-
-// middleware/index.js (optional - for easier imports)
-module.exports = {
-  requireLogin: require('./requireLogin'),
-  checkMaintenance: require('./checkMaintenance'),
-  requireLoginAndMaintenance: require('./requireLoginAndMaintenance')
-};
