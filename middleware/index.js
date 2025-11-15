c5560c6d8f7f44789268ed1b79fd75fa02f3219e
Archer <archertrenwith@gmail.com>
2025-11-15 11:30:35 +1100

    Delete middleware/index.js

--- middleware/index.js
+++
@@ -1,7 +0,0 @@
-
-// middleware/index.js (optional - for easier imports)
-module.exports = {
-  requireLogin: require('./requireLogin'),
-  checkMaintenance: require('./checkMaintenance'),
-  requireLoginAndMaintenance: require('./requireLoginAndMaintenance')
-};
