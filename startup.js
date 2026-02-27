2273da18b48078d91e9e4b27210f5c57bb17ff66
Archer <archertrenwith@gmail.com>
2026-02-28 10:41:09 +1100

    Update startup.js

--- startup.js
+++ startup.js
@@ -141,7 +141,7 @@ async function main() {
   });
 
   // Start
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 4000;
   const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`\n🚀 Server running on port ${PORT}`);
   });
