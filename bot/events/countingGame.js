e1c309cabaac52e1f0cbe030c6a315ac9abc63bf
Archer <archertrenwith@gmail.com>
2026-04-13 10:23:57 +1000

    Update countingGame.js

--- bot/events/countingGame.js
+++ bot/events/countingGame.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
-const FREE_PASS_MILESTONE = 100;
+const FREE_PASS_MILESTONE = 50;
 
 module.exports = (client) => {
     const lastResponseSent = { messageId: null, timestamp: 0 };
